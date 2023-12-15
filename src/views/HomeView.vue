ab0931d5fab64464f2c32e4699702178411b5f37
Liam Moore <lmoore@praxes.ca>
2023-12-15 13:18:14 +0000

    Updated homeview animation and fixed bio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,12 +17,18 @@
                   <span ref="im">I'm </span>
                   <span class="text-transparent bg-clip-text bg-gradient-to-br from-primary to-primary-focus">Liam Moore</span>
                 </kinesis-element>
-                <div ref="bio" class="fixed mt-6 ml-20">
+                <div ref="bio" class="fixed mt-6 ml-20 card-bg">
                   <div class="px-5 pb-4 pt-2 card-bg items-center border border-neutral rounded-lg shadow lg:flex-row lg:max-w-xl z-40">
-                    <h5 class="font-extrabold text-2xl py-1 tracking-wider text-secondary">TLDR:</h5>
-                    <p>
-                      Hello this is a brief TLDR about Liam. Blah blah blah, he kinda knows how to code but not really.
+                    <h5 class="font-extrabold text-xl py-1 tracking-wider text-secondary">TLDR</h5>
+                    <p class="pb-2">
+                      I'm a software developer from Nova Scotia Canada specializing in UI/UX design and development.
+                      Currently working full time as a Full Stack developer with Vue3, TypeScript and Quasar. 
                     </p>
+                    <!-- <h5 class="font-extrabold text-xl py-1 tracking-wider text-secondary">Socials</h5>
+                    <p>
+                      I'm a software developer from Nova Scotia Canada specializing in UI/UX design and development.
+                      Currently working full time as a Full Stack developer with Vue3, TypeScript and Quasar. 
+                    </p> -->
                   </div>
                 </div>
               </div>
@@ -78,7 +84,7 @@
           <!-- education list -->
           <div class="space-y-4">
             <p :strength="5" class="header-bg font-extrabold text-5xl py-4 tracking-wider z-40 text-secondary">Education 🎓</p>
-            <education-card 
+            <time-period-card 
               class="col-span-1"
               title="Bachelor of Applied Computer Science" 
               dateRange="2017 - 2022" 
@@ -90,11 +96,18 @@
           <!-- work experience list -->
           <div class="space-y-4">
             <p :strength="5" class="header-bg font-extrabold text-5xl py-4 tracking-wider z-40 text-accent">Work Experience 📈</p>
-            <education-card 
+            <time-period-card 
               class="col-span-1"
               title="Full Stack Software Developer" 
               dateRange="2022 - present" 
               subtitle="Praxes Medical Group"
+              :theme="theme"
+            />
+            <time-period-card 
+              class="col-span-1"
+              title="Assistant Platform Development Manager" 
+              dateRange="2019 - 2022" 
+              subtitle="McIntyre Media Inc."
               :theme="theme"
             />
           </div>
@@ -113,11 +126,11 @@ import ParticleBackground from '../components/ParticleBackground.vue';
 // @ts-ignore
 import { KinesisContainer, KinesisElement } from 'vue-kinesis';
 import ProjectCard from '../components/cards/ProjectCard.vue';
-import EducationCard from '../components/cards/EducationCard.vue';
+import TimePeriodCard from '../components/cards/TimePeriodCard.vue';
 
 export default defineComponent({
   name: 'HomeView',
-  components: { EducationCard, KinesisContainer, KinesisElement, ParticleBackground, ProjectCard },
+  components: { TimePeriodCard, KinesisContainer, KinesisElement, ParticleBackground, ProjectCard },
   props: {
     theme: {
       type: String,
@@ -185,10 +198,11 @@ export default defineComponent({
     bioFade(ref: HTMLElement) {
       gsap.from(ref as HTMLElement, {
         scrollTrigger: {
-          trigger: '.project-box',
-          toggleActions: "restart none reverse reverse"
+          trigger: this.$refs.projectsTitle as HTMLElement,
+          toggleActions: "restart none reverse reverse",
+          start: "bottom bottom",
+          scrub: true
         },
-        delay: 0.5,
         y: 70,
         opacity: 0, // Start with opacity 0
         duration: 0.5, // Animation duration
