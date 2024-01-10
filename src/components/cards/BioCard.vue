0df746f80b08ddebb721d958a5363f4a06f9b340
Liam Moore <mooreliama@gmail.com>
2024-01-09 23:19:40 -0400

    - made card borders more consistent

--- src/components/cards/BioCard.vue
+++ src/components/cards/BioCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="'px-5 pb-4 pt-2 border rounded-lg shadow-lg lg:flex-row mb-4 card-bg z-40' + (theme === 'winter' ? ' border-gray-400' : ' border-zinc-900')" >
+  <div :class="'px-5 pb-4 pt-2 border rounded-lg shadow-lg lg:flex-row mb-4 card-bg z-40' + (theme === 'winter' ? ' border-gray-300' : ' border-zinc-900')" >
     <h5 class="font-extrabold text-xl py-1 tracking-wider text-secondary">TLDR</h5>
     <p class="pb-2">
       I'm a software developer from Nova Scotia Canada specializing in UI/UX design and development.
