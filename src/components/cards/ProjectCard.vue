edc6e86b05f07d38f134cf5a482c3b40b381e669
Liam Moore <lmoore@praxes.ca>
2024-01-13 20:59:47 -0400

    - removed node modules

--- src/components/cards/ProjectCard.vue
+++ src/components/cards/ProjectCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <kinesis-container>
-    <a href="#" :class="'flex card-bg items-center border shadow-md rounded-lg flex-row z-40 p-2 space-x-4 space-y-4 night-card-bg' + (theme === 'winter' ? ' border-gray-300' : ' border-zinc-900')">
+  <kinesis-container @click="handleClick">
+    <a :class="'flex card-bg items-center border shadow-md rounded-lg flex-row z-40 p-2 space-x-4 space-y-4 night-card-bg' + (theme === 'winter' ? ' border-gray-300' : ' border-zinc-900')">
       <kinesis-element :strength="5" class="lg:w-1/3 card-img">
         <slot name="thumbnailSlot"></slot>
       </kinesis-element>
@@ -47,7 +47,9 @@ export default defineComponent({
     // console.log(this.thumbnailArr);
   },
   methods: {
-
+    handleClick() {
+      console.log('hello')
+    }
   },
 });
 </script>
