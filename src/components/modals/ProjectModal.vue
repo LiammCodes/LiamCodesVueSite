16068963d5a156fd64a87f30c3fbebaea25ed86b
Liam Moore <lmoore@praxes.ca>
2024-01-22 18:21:02 -0400

    - fixed modal not closing when clicked outside

--- src/components/modals/ProjectModal.vue
+++ src/components/modals/ProjectModal.vue
@@ -1,13 +1,15 @@
 <template>
-  <!-- You can open the modal using ID.showModal() method -->
   <dialog ref="projectModal" class="modal" :class="{ 'modal-open': showModal }">
-    <div class="modal-box">
+    <div class="modal-box w-11/12 max-w-5xl">
       <form method="dialog">
         <button @click="handleModalClose" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
       </form>
       <h3 class="font-bold text-lg">Hello!</h3>
-      <p class="py-4">Press ESC key or click on ✕ button to close</p>
+      <p class="py-4">Press ESC key or click outside to close</p>
     </div>
+    <form method="dialog" class="modal-backdrop">
+      <button @click="handleModalClose">close</button>
+    </form>
   </dialog>
 </template>
 
