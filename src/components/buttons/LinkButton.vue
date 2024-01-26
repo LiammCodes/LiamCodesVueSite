90b6d7eecbd2e992f3017588dd9926b66600bae2
Liam Moore <lmoore@praxes.ca>
2024-01-25 20:16:40 -0400

    - modified link button component

--- src/components/buttons/LinkButton.vue
+++ src/components/buttons/LinkButton.vue
@@ -1,5 +1,13 @@
 <template>
-
+  <a 
+    role="button" 
+    href="https://github.com/LiammCodes" 
+    target="_blank" 
+    :class="'btn btn-outline bio-card-bg bg-opacity-10 grow mb-4 btn-default bg-gray-500'"
+  >
+    <font-awesome-icon :icon="['fab', 'github']" size="xl" />
+    Github
+  </a>
 </template>
 
 <script lang="ts">
@@ -8,7 +16,14 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'LinkButton',
-  
+  props: {
+    icon: () => Array<String>,
+    text: String,
+    customClassList: String,
+    textColor: String,
+    borderColor: String,
+    backgroundColor: String
+  }
 });
 
 </script>
