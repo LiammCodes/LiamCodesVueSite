0d4c3adb96119d0523ac34ec6614e00b3d90ae34
Liam Moore <lmoore@praxes.ca>
2024-02-02 00:28:54 -0400

    Actually fixed link buttons this time (rip LinkButton component lmao)

--- src/components/cards/BioCard.vue
+++ src/components/cards/BioCard.vue
@@ -7,24 +7,37 @@
     </p>
   </div>
   <div class="flex flex-col lg:flex-row lg:space-x-3">
-    <link-button 
-      text="Github" 
-      :icon="['fab', 'github']"
-      customClassList="btn-default bg-gray-500"
-      link="https://github.com/LiammCodes"
-    />
-    <link-button 
-      text="Instagram" 
-      color="pink"
-      :icon="['fab', 'instagram']"
-      link="https://instagram.com/liammoore99"
-    />
-    <link-button 
-      text="Twitch" 
-      color="purple"
-      :icon="['fab', 'twitch']"
-      link="https://twitch.tv/liama6"
-    />
+
+    <a 
+      role="button" 
+      href="https://github.com/LiammCodes" 
+      target="_blank" 
+      class="btn btn-outline bio-card-bg bg-opacity-10 grow mb-4 btn-default bg-gray-500"    
+    >
+      <font-awesome-icon :icon="['fab', 'github']" size="xl" />
+      Github
+    </a>
+
+    <a 
+      role="button" 
+      href="https://instagram.com/liammoore99" 
+      target="_blank" 
+      class="btn btn-outline bio-card-bg bg-opacity-10 grow mb-4 text-pink-600 bg-pink-600 hover:bg-pink-500 hover:border-pink-600"    
+    >
+      <font-awesome-icon :icon="['fab', 'instagram']" size="xl" />
+      Instagram
+    </a>
+
+    <a 
+      role="button" 
+      href="https://twitch.tv/liama6" 
+      target="_blank" 
+      class="btn btn-outline bio-card-bg bg-opacity-10 grow mb-4 text-purple-600 bg-purple-600 hover:bg-purple-500 hover:border-purple-600"    
+    >
+      <font-awesome-icon :icon="['fab', 'twitch']" size="xl" />
+      Twitch
+    </a>
+
   </div>
 </template>
 <script lang="ts">
